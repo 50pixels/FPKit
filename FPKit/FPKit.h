ce7f22924969f1d88c3fb8f48606f18cfa60407b
Alvise Susmel <alvise@MacBookPro.local>
2012-09-25 12:43:02 +0100

    Initial Commit

---
+++ FPKit/FPKit.h
@@ -0,0 +1,13 @@
+//
+//  FPKit.h
+//  FPKit
+//
+//  Created by Alvise Susmel on 25/09/2012.
+//  Copyright (c) 2012 Fifty Pixels Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FPKit : NSObject
+
+@end
